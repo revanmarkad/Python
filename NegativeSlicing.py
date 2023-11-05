15b458486f9b6767d1676f2f81cfef996f5bc698
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2023-11-05 10:42:36 +0530

    Add files via upload

---
+++ NegativeSlicing.py
@@ -0,0 +1,18 @@
+place = "madhi"
+
+print(len(place))  # 5
+
+print(place[: len(place) - 3])  # ma
+
+print(
+    place[-3:-1]
+)  # len of place -3 =2 // len of place -1 =4 //  conclusion is 2:4  then start from second index and go to third index thats why ans is 'dh'
+
+
+# Quick Quize;
+
+
+name = "Revannath"
+
+print(len(name))  # 9
+print(name[-4:-2])  # 9-4=5 // 9-2=7  / = 5:7 ans= na
---
+++ stringSlicing.py
@@ -0,0 +1,3 @@
+names = "revan,markad"
+print(len(names))
+print(names[0:4])  # including 0 but not 4
