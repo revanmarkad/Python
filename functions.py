f1e5076dea80642212a330ce9292722055b3e596
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2023-11-30 15:58:12 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,24 @@
+def calculate(a, b):
+    mean = (a * b) / (a + b)
+    print(mean)
+
+
+def isGreater(a, b):
+    if a > b:
+        print("first num  is grater", a)
+    else:
+        print("second num  is grater or equal", b)
+
+def isLesser(a,b):
+    pass # pass means i do this work after some time
+c = 8
+d = 6
+
+isGreater(c, d)
+calculate(c, d)
+
+
+r = 4
+u = 6
+isGreater(r, u)
+calculate(r, u)
