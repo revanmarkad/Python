085aebd5ca8dd9c01203835e810ffa3a6c5372d0
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2023-12-01 15:34:20 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,25 @@
+# list=[1,2,3,4]
+# print(list)
+# print(type(list))
+
+
+# # 2
+
+
+# marks=[88,99,54,74]
+# print(marks[0])
+
+
+# for x in marks:
+#  print(x)
+
+
+
+
+# 3
+
+
+# names=['revan','adii','madf','sultr']
+# print(names)
+# for i in names:
+#     print(i)
