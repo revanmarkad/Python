e3be61d3e2348e26517fd7d22e5b1c48aef29047
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2023-12-01 13:45:20 +0530

    Add files via upload

---
+++ function_Argument.py
@@ -0,0 +1,17 @@
+def name(fname,mname='Ashok',lname='Markad'):
+    print("Hello,",fname,mname, lname)
+
+
+name("Revannath")
+
+
+# # 2
+
+
+# def name(**name):
+#      print(type(name))
+#      print("Hello,", name["fname"], name["mname"], name["lname"])
+
+
+# print(mname="ashok", lname="markad", fname="Revan")
+ 
