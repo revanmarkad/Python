03a1f836cbfd47391170781fc4c28d69e80058da
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2023-12-06 06:25:41 +0530

    Add files via upload

---
+++ Dictionary_methods.py
@@ -0,0 +1,18 @@
+emp={'Revan':99.9,'alwas':88,'sonal':18,'Reruvatu':14,'dalver':9}
+
+emp2={'kiran':17,'mant':8,'deve':18,'arched':4}
+
+
+emp.update(emp2)# all emp 2 id are added in emp ;
+
+# emp.clear() #dlt all keys and value;
+
+emp.pop("alwas")  #using pop we can dlt any value;
+
+emp.popitem()# using popitem we can dlt last key_value;
+
+#del emp      # del dlt the dictionary 
+
+
+
+print(emp)
