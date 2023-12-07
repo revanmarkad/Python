6581d75c5972f84e401ea2467a634c1988cf5707
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2023-12-07 22:15:18 +0530

    Add files via upload

---
+++ Revan.py
@@ -0,0 +1,5 @@
+def welcome():
+    print("for importing purpose")
+
+
+revan='markad'
---
+++ importing_in_python.py
@@ -0,0 +1,17 @@
+# import pandas
+# pandas.read_csv()
+
+#2
+# import math
+
+# print(math.floor(4.2343))
+
+# print(math.sqrt(9))
+
+
+#3
+
+# from Revan import * #1
+from Revan import welcome,revan #2 
+welcome()
+print(revan)
