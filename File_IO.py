fba95fcd3feb903102c5cf171e387506c71e20e0
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2023-12-10 22:36:32 +0530

    Add files via upload

---
+++ File_IO.py
@@ -0,0 +1,21 @@
+
+# REDAING A FILE
+
+# file=open("File.txt",'r')
+# # print(file)
+
+# text=file.read()
+# print(text)
+# file.close()
+
+
+
+# WRITING A FILE
+
+
+from msilib.schema import File
+
+
+file=open('File.txt','a')
+file.write("file test!!!!!!!!!!!!!!!!!!!1\n")
+file.close()
