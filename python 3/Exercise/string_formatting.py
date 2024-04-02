1a5e645ac00107261d86caa89db2c4583ad9e393
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2024-04-03 00:04:20 +0530

    Add files via upload

---
+++ python 3/Exercise/string_formatting.py
@@ -0,0 +1,7 @@
+num1=int(input('Enter first Number:'))
+num2=int(input('Enter Second Number:'))
+num3=int(input('Enter Third Number:'))
+
+avg=(num1+num2+num3)/3
+
+print(f"You given three number addition is {avg}")
