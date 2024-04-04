8957bc028622e33d92ab8dbfe04c38aadb1bab05
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2024-04-04 10:54:47 +0530

    Update plus_minus.py

--- HackerRank/plus_minus.py
+++ HackerRank/plus_minus.py
@@ -1,3 +1,7 @@
+# Question:
+# Given an array of integers, calculate the fractions of its elements that are positive, negative, and zeros.
+# Print the decimal value of each fraction with 6 decimal places.
+
 def plusMinus(arr):
     positive = negative = zeros = 0
     for i in arr:
@@ -8,6 +12,7 @@ def plusMinus(arr):
         else:
             zeros += 1
 
+    # Calculate the fractions and print them with 6 decimal places
     print("{:.6f}".format(positive/len(arr)))
     print("{:.6f}".format(negative/len(arr)))
     print("{:.6f}".format(zeros/len(arr)))
