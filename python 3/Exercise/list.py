78b997ce877d85ae2bcd0dcae9509a9ba7f0b76d
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2024-04-10 22:33:00 +0530

    Add files via upload

---
+++ python 3/Exercise/list.py
@@ -0,0 +1,20 @@
+ 
+list1 = [32, 45, 76, 21]
+
+# Append the value 12 to the end of list1
+list1.append(12)
+
+# Insert the value 99 at index 2 in list1
+list1.insert(2, 99)
+
+ 
+list2 = [11, 22, 33, 44]
+
+# Remove the value 32 from list1
+list1.remove(32)
+
+# Extend list1 with the elements from list2
+list1.extend(list2)
+
+# Print the modified list1, which now contains elements from both list1 and list2
+print(list1)
