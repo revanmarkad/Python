059db639e70178afd40f5c1ba6a7ac98736e7ae7
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2024-04-10 22:35:15 +0530

    Add files via upload

---
+++ python 3/Exercise/list_inside_list.py
@@ -0,0 +1,11 @@
+my_list=[[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+
+
+for list1 in my_list:
+    for sublist in list1:
+        print(sublist,end=" ")
+print()
+
+# Print the list containing numbers from 1 to 10
+number = list(range(1, 11))
+print(number)
